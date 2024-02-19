df388b44cfe7916dee641e9e1a40fb1569f407b7
Walter Branning <walter.d.branning@icloud.com>
2024-02-18 23:10:50 -0600

    Finished intial trial run of ratatui

---
+++ src/app.rs
@@ -0,0 +1,31 @@
+
+
+#[derive(Debug, Default)]
+pub struct App {
+    pub counter: i64,
+    pub should_quit: bool,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn tick(&self) {}
+
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+
+    pub fn increment_count(&mut self) {
+        if let Some(res) = self.counter.checked_add(1) {
+            self.counter = res;
+        }
+    }
+
+    pub fn decrement_count(&mut self) {
+        if let Some(res) = self.counter.checked_sub(1) {
+            self.counter = res;
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,34 @@
-use crossterm::{
-    event::{self, KeyCode, KeyEventKind}, execute, terminal::{
-        self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen
+pub mod app;
+pub mod event;
+pub mod tui;
+pub mod ui;
+pub mod update;
+
+use app::App;
+use color_eyre::Result;
+use crossterm::terminal;
+use event::{Event, EventHandler};
+use ratatui::{backend::{self, CrosstermBackend}, Terminal};
+use tui::Tui;
+use update::update;
+
+fn main() -> Result<()> {
+    let mut app = App::new();
+    let backend = CrosstermBackend::new(std::io::stderr());
+    let terminal = Terminal::new(backend)?;
+    let events = EventHandler::new(250);
+    let mut tui = Tui::new(terminal, events);
+    tui.enter()?;
+
+    while !app.should_quit {
+        tui.draw(&mut app)?;
+        match tui.events.next()? {
+            Event::Tick => {}
+            Event::Key(key_event) => update(&mut app, key_event),
+            Event::Mouse(_) => {}
+            Event::Resize(_, _) => {}
+        };
     }
-};
-
-use anyhow::Result;
-use std::fmt::format;
-
-use ratatui::{
-    prelude::{CrosstermBackend, Stylize, Terminal},
-    widgets::Paragraph,
-};
-
-
-struct App {
-    counter: i64,
-    should_quit: bool,
-}
-
-fn startup() -> Result<()> {
-    enable_raw_mode()?;
-    execute!(std::io::stderr(), EnterAlternateScreen)?;
-    Ok(())
-}
-
-fn shutdown() -> Result<()> {
-    execute!(std::io::stderr(), LeaveAlternateScreen)?;
-    disable_raw_mode()?;
-    Ok(())
-}
-
-fn main() -> Result<()>{
-    startup()?;
-
-    let mut terminal = Terminal::new(CrosstermBackend::new(std::io::stderr()))?;
-    let mut counter = 0;
-
-    loop {
-        terminal.draw(|f|{
-            f.render_widget(Paragraph::new(format!("Counter: {counter}")), f.size());
-        })?;
-
-        if crossterm::event::poll(std::time::Duration::from_millis(250))? {
-            if let crossterm::event::Event::Key(key) = crossterm::event::read()? {
-                if key.kind == crossterm::event::KeyEventKind::Press {
-                    match key.code {
-                        crossterm::event::KeyCode::Char('j') => counter +=1,
-                        crossterm::event::KeyCode::Char('k') => counter -=1,
-                        crossterm::event::KeyCode::Char('q') => break,
-                        _ => {},
-                    }
-                }
-            }
-        }
-    }
-
-    shutdown()?;
+    tui.exit()?;
     Ok(())
 }
