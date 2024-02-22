3185a529fea43e02b8b6c703257b3232b685df41
Walter Branning <walter.d.branning@icloud.com>
2024-02-21 22:44:25 -0600

    Added Bottom panel

--- src/ui.rs
+++ src/ui.rs
@@ -7,26 +7,35 @@ use ratatui::{
     }
 };
 
-pub fn ui(frame: &mut Frame) {
+pub fn ui(frame: &mut Frame) {;
+
+    let main_window_split = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Percentage(100), Constraint::Percentage(15)])
+        .split(frame.size());
+
+    // let main_window = Layout::default()
+    //     .direction(Direction::Vertical)
+    //     .constraints([Constraint::Percentage(90), Constraint::Percentage(0)])
+    //     .split(controls[1]);
 
     frame.render_widget(
         Block::new()
-            .title("The Forgotten City Of Xeo")
+            .title(" The Forgotten City Of Xeo ")
             .title_alignment(Alignment::Center)
             .borders(Borders::ALL)
             .style(Style::default().fg(Color::Yellow))
             .border_type(BorderType::Rounded),
-        frame.size());
+        main_window_split[0]);
 
-    let header = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(5), Constraint::Percentage(100)])
-        .split(frame.size());
-
-    let main_window = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([Constraint::Percentage(90), Constraint::Percentage(0)])
-        .split(header[1]);
+    frame.render_widget(
+            Block::new()
+                .title(" Controls ")
+                .title_alignment(Alignment::Center)
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::Yellow))
+                .border_type(BorderType::Rounded),
+            main_window_split[1]);
     
     // render_window(frame, header[0], "Game header");
     // render_window(frame, main_window[0], "Main");
